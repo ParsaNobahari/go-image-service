4e6994d5eb6c7251803c15fa03230abe89aa1d88
ParsaNobahari <parsa.nobahari@gmail.com>
2023-07-15 20:07:18 +0330

    connecting to rabbitmq

--- main.go
+++ main.go
@@ -13,5 +13,9 @@ func failOnError(err error, msg string) {
 }
 
 func main() {
-    fmt.Println("Hello")
+
+    conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+    failOnError(err, "Failed to connect to RabbitMQ")
+    defer conn.Close()
+    
 }
