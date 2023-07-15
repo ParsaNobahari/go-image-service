d6607440f4bc8e86e76e8ba9972b2eb81d2fafd8
ParsaNobahari <parsa.nobahari@gmail.com>
2023-07-15 20:08:55 +0330

    created new channel

--- main.go
+++ main.go
@@ -17,5 +17,9 @@ func main() {
     conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
     failOnError(err, "Failed to connect to RabbitMQ")
     defer conn.Close()
-    
+
+    ch, err := conn.Channel()
+    failOnError(err, "Failed to open a channel")
+    defer ch.Close()
+
 }
